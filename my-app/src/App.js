4406de9526ea393f10b8b1636e803e489ae94d15
Rachael Babister <rachaelbabister@gmail.com>
2024-06-10 13:02:12 +0000

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps.js';
+
+function App() {
+  return (
+    <div className="App">
+      <FunctionalGreetingWithProps />
+    </div>
+  );
+}
+
+export default App;
